e101d4c75d6cf96c674f451aaf4d1f07e86c574a
naraton <nara.ton39@gmail.com>
2025-01-30 16:31:45 +0700

    Update main_message.dart

--- lib/page/main_message.dart
+++ lib/page/main_message.dart
@@ -144,7 +144,7 @@ class _MainMessageState extends State<MainMessage> {
                                   //color: const Color.fromARGB(255, 172, 138, 250),
                                   shape: BoxShape.circle,
                                   border: Border.all(
-                                    color: Colors.deepPurple,
+                                    color: Colors.indigo[900]!,
                                     width: 2.0,
                                   ),
                                 ),
